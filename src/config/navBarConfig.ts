41e8fce7cd5fae87e22a095daf949c059c38fb83
ahmednagj100-sketch <ahmednagj100@gmail.com>
2026-07-25 03:21:08 +0300

    Remove C++ dropdown menu from navigation bar

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -170,18 +170,5 @@ export const navBarConfig: NavBarConfig = {
     // 项目页面
     LinkPreset.Projects,
 
-    // C++ section
-    {
-      name: "C++",
-      url: "/cpp/",
-      icon: "material-symbols:code",
-      children: [
-        {
-          name: "Introduction to C++",
-          url: "/cpp/introduction-to-cpp/",
-          icon: "material-symbols:book",
-        },
-      ],
-    },
   ],
 };
